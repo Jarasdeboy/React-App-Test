3830dc386aa66edda87bec3a180b9bb37e8e52af
Jarasdeboy <raul.jaramillo@outlook.com>
2023-05-25 08:52:56 -0600

    Updates

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.png';
 import './App.css';
 
 function App() {
-	const rutaImagen = 'https://scontent.fslw1-1.fna.fbcdn.net/v/t31.18172-8/21125532_1489758431063173_6890971843348264248_o.jpg?_nc_cat=103&ccb=1-7&_nc_sid=174925&_nc_ohc=Qcnp3zl2_7EAX8TvYPZ&_nc_ht=scontent.fslw1-1.fna&oh=00_AfDdSagTtXLZX-bgl8SqjUUoKPNReVP37kYm_tVDZppXrQ&oe=649668BF';
+	const rutaImagen = 'https://images01.nicepagecdn.com/a1389d7bc73adea1e1c1fb7e/d188e82e3328502aba1161b3/young-confident-woman-with-natural-light-make-up-holding-hands-waist-looking-left-empty-copy-space-staring-logo-smiling-standing-white-wall1.png';
 
 	return (
 		<div className='Contenedor'>
@@ -20,9 +20,9 @@ function App() {
 				<div className='user-info'>
 					<div className='box-user-info'>
 						<div className='box-info'>
-							<div className='BigText'>How to be a Lego Manager</div>
+							<div className='BigText'>How to Get Consulting Clients</div>
 						</div>
-						<p className='parrafoUser'>You need to have a big dick to satisfy lego girls.</p>
+						<p className='parrafoUser'>Nibh venenatis cras sed felis eget velit aliquet sagittis id. Tellus pellentesque eu tincidunt tortor aliquam nulla.</p>
 					</div>
 					<div className='box-user-img'>
 						<img className='ImagenP' src ={rutaImagen} alt="Jaime foto"/>
