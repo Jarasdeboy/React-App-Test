cb0540d1d0e80d41eb63ac55a054c33b331b83c9
Jarasdeboy <raul.jaramillo@outlook.com>
2023-05-31 15:02:01 -0600

    updates

--- src/App.js
+++ src/App.js
@@ -19,12 +19,12 @@ function App() {
 			<section className='section'>
 				<div className='user-info'>
 					<div className='box-user-info'>
-						<div className='box-info'>
+						<div className='box-info zoom-in-out-box'>
 							<div className='BigText'>How to Get Consulting Clients</div>
 						</div>
 						<p className='parrafoUser'>Nibh venenatis cras sed felis eget velit aliquet sagittis id. Tellus pellentesque eu tincidunt tortor aliquam nulla.</p>
 						<p className='subparrafo'> Article evident arrived express highest men did boy. Mistress sensible entirely am so. Quick can manor smart money hopes worth too. Comfort produce husband boy her had hearing. Law others theirs passed but wishes. You day real less till dear read. Considered use dispatched melancholy sympathize discretion led. Oh feel if up to till like. He an thing rapid these after going drawn or. Timed she his law the spoil round defer. </p>
-						<a href="#" className='BtnReadme'>read more</a>
+						<a href="#" className='BtnReadme link-animado'>read more</a>
 					</div>
 					<div className='box-user-img'>
 						<img className='ImagenP' src ={rutaImagen} alt="Jaime foto"/>
